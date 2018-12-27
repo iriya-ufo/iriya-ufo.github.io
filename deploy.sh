77c49689215151cd5742fe1de86963cbb95d7393
Naoto Inoue <info@cstap.com>
2018-12-27 12:16:23 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,23 @@ then
     exit 1;
 fi
 
-echo "\U2705 Deleting old publication...\n"
+echo -e "*** Deleting old publication...\n"
 rm -rf public
 
-echo "\U2705 Generating site...\n"
+echo -e "*** Generating site...\n"
 hugo
 
-echo "\U2705 Adding CNAME...\n"
+echo -e "*** Adding CNAME...\n"
 echo iriya-ufo.net >> public/CNAME
 
-echo "\U2705 git add && git commit...\n"
+echo -e "*** git add && git commit...\n"
 git add public/
 git commit -m "release `date '+%Y-%m-%d %H:%M'`"
 
-echo "\U2705 Updating master branch...\n"
+echo -e "*** Updating master branch...\n"
 git subtree push --prefix public/ origin master
 
-echo "\U2705 Updating source branch...\n"
+echo -e "*** Updating source branch...\n"
 git push origin source
 
-echo "\U2705 Success Deploy !!!\n"
+echo "*** Success Deploy !!!"
